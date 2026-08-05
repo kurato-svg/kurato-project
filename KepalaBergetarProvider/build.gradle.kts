3ac19d6d0c0cf7687044cb4939c2f36639c16775
kurato-svg <kuratotv@gmail.com>
2026-08-05 23:15:04 +0800

    Create build.gradle.kts

---
+++ KepalaBergetarProvider/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "KepalaBergetar — Drama Melayu"
+    language = "ms"
+    authors = listOf("Kurato")
+
+    status = 3
+
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie"
+    )
+
+    iconUrl = "https://kepalabergetar9.pro/favicon.ico"
+}
