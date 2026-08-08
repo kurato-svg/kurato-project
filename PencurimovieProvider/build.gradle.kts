644e2537b993f37cbed05f1921ab95c923d4977a
kurato-svg <kuratotv@gmail.com>
2026-08-08 20:36:01 +0800

    Create build.gradle.kts

---
+++ PencurimovieProvider/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Pencurimovie — Streaming Film & Serial"
+    language    = "id"
+    authors     = listOf("Luckez")
+    status = 1
+    tvTypes = listOf(
+		"Movie", "TvSeries"
+	)
+}
