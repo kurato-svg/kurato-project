e712ae949b186d12f4a8ebd933d5efc352c7daf9
kurato-svg <kuratotv@gmail.com>
2026-08-08 15:59:43 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,17 @@
 rootProject.name = "CloudstreamPlugins"
 
-// Temporary: build only AnichinV2Provider
-//include("AnichinXProvider")
+// This file sets what projects are included. All new projects should get automatically included unless specified in "disabled" variable.
+val disabled = listOf<String>()
 
- Auto-discovery disabled temporarily
- val disabled = listOf<String>()
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
+    }
+}
 
- File(rootDir, ".").eachDir { dir ->
-     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-         include(dir.name)
-     }
- }
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
 
- fun File.eachDir(block: (File) -> Unit) {
-     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
- } 
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
