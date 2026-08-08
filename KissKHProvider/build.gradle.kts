657ba7df55ca0cd6f180e37ae89131c08f49fb78
kurato-svg <kuratotv@gmail.com>
2026-08-08 16:57:19 +0800

    Update build.gradle.kts

--- KissKHProvider/build.gradle.kts
+++ KissKHProvider/build.gradle.kts
@@ -1,10 +1,9 @@
-version = 4
-
+version = 5
 cloudstream {
     authors     = listOf("Luckez")
     language    = "en"
     description = "Watch drama online in high quality. Free download high quality drama. Various formats from 240p to 720p HD (or even 1080p). Feel Free To Watch!"
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("AsianDrama") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
+    status  = 1
+    tvTypes = listOf("AsianDrama")
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://kisskh.id/&size=256"
 }
